b3ed6c739ae48df371c778b3e80b2f9cd0b79df2
Ian Pollack <ianjpollack@gmail.com>
2018-03-19 19:58:02 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -2,11 +2,11 @@
 
 def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  counter = 0
+  counter = 1
   loop do
     counter += 1
     puts phrase
-    break if counter >= number_of_times
+    break if counter > number_of_times
   end
 end
 
